bb0867ef24ea71498030991ed5871854639ebfd9
yangzhen <1784054000@qq.com>
2015-10-19 17:28:15 +0800

    初始化组件

---
+++ doExtLib/do_MarqueeLabel_IView.h
@@ -0,0 +1,25 @@
+//
+//  do_MarqueeLabel_UI.h
+//  DoExt_UI
+//
+//  Created by @userName on @time.
+//  Copyright (c) 2015年 DoExt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@protocol do_MarqueeLabel_IView <NSObject>
+
+@required
+//属性方法
+- (void)change_fontColor:(NSString *)newValue;
+- (void)change_fontSize:(NSString *)newValue;
+- (void)change_fontStyle:(NSString *)newValue;
+- (void)change_text:(NSString *)newValue;
+- (void)change_textAlign:(NSString *)newValue;
+- (void)change_textFlag:(NSString *)newValue;
+
+//同步或异步方法
+
+
+@end
